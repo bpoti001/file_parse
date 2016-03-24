2a4aa945f4dd21309301613151cd242ef362fe2c
bpoti001 <bpoti001@odu.edu>
2016-03-24 14:22:26 -0400

    push

---
+++ file.py
@@ -0,0 +1,15 @@
+fi = open("D://present_locations.txt",'r') #file reader
+present=[] # a list to store all locations in script
+for line in fi:
+    present.append(line.strip()) #strips is \n from a line
+fi.close()
+prefix ='string_to_append_in_start'
+postfix ='string_to_append_in_end'
+updated=[] #list to store new locations
+for i in present:
+    updated.append(prefix+' '+i+' '+postfix) # + is used to concatinate strings in python empyt '' indecates space
+
+fo = open('D://udpated_file_locations.txt','w')
+for i in updated:
+    fo.write('%s\n' %(i))
+fo.close()
